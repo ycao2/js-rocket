4960a3de7e5b231ff351adeddcb80cb6a6d1c48d
Yang Cao <ycao2@luc.edu>
2016-02-18 15:27:59 -0600

    Made some car stuff happen!

---
+++ explanations.js
@@ -0,0 +1,45 @@
+//Explanations
+var favColor = 'blue';
+var myFavColors = ['blue','red','green'];
+var numOfFavColors = 3;
+var hasGotFavColors = true;
+var richObject = {
+	firstName: 'Rich',
+	lastName: 'Armstrong',
+	favColors: ['blue','red','green'],
+	yearsAlive:103,
+	isMale: true
+};
+
+richObject.isMale
+//================================================
+var whatIsMyFavColor = function (){
+	return true;
+	
+	return false;
+
+	return 'blue';
+}
+var doSomething = function (){
+	console.log("do somthing");
+}
+
+var area = function(width, height){
+	return width * height;
+}
+
+var fullname =  function(firstname, secondname){
+	return firstname + ' '+ secondname;
+}
+//================================================
+
+var name1 = 'Rich';
+var name2 = 'Bob';
+
+if( name1 < name2) {
+	//do something
+	alert('true!!!');
+}else{
+	alert('false!!!');
+}
+
